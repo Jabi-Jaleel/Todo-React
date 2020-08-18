883333d9713e46a59fab2f673339c0da68a63c8e
Jabir <jabirasd@gmail.com>
2020-08-18 23:01:52 +0530

    changed font size

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h3 style={{ textAlign: "center", fontSize: "70px" }}>About Component</h3>
+      <h3 style={{ textAlign: "center", fontSize: "80px" }}>About Component</h3>
     </div>
   );
 }
