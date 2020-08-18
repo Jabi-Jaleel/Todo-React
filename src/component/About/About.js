4137f8547aa64f3507ec3f337d516d4ccd291694
Jabir <jabirasd@gmail.com>
2020-08-18 23:42:16 +0530

    about content

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h3 style={{ textAlign: "center", fontSize: "80px" }}>About Component</h3>
+      <h3 style={{ textAlign: "center", fontSize: "70px" }}>About Component</h3>
+      <p style={{ textAlign: "center", fontSize: "20px" }}>
+        This project was created to know the basic of react
+      </p>
     </div>
   );
 }
