58782b7ccf590e00d136d5db66260c3f627a089b
Jabir <jabirasd@gmail.com>
2020-08-19 01:08:45 +0530

    Updates

---
+++ precache-manifest.21986c272434d00c9fce2379c42afab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3eeb71066721f7b48e77d65a98d6574",
+    "url": "/Todo-React/index.html"
+  },
+  {
+    "revision": "685531388a169d34c9fc",
+    "url": "/Todo-React/static/css/main.91306872.chunk.css"
+  },
+  {
+    "revision": "1b89a7576dd0cdfa8094",
+    "url": "/Todo-React/static/js/2.1a2fec60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-React/static/js/2.1a2fec60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685531388a169d34c9fc",
+    "url": "/Todo-React/static/js/main.f8fd5c98.chunk.js"
+  },
+  {
+    "revision": "81ab99712b897acf60b9",
+    "url": "/Todo-React/static/js/runtime-main.c523feda.js"
+  }
+]);
